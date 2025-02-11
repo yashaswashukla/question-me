4e268c3924d0d731800d7f136ae245600985fdd9
Yashaswa Shukla <yashaswashukla@gmail.com>
2025-02-11 10:11:40 +0530

    Added delete route

---
+++ src/app/api/delete-message/[messageId]/route.ts
@@ -0,0 +1,40 @@
+import dbConnect from "@/lib/dbConnect";
+import { getServerSession, User } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import UserModel from "@/model/User";
+
+export default async function DELETE(
+  req: Request,
+  { params }: { params: { messageId: string } }
+) {
+  const messageId = params.messageId;
+  await dbConnect();
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+
+  if (!session || !session.user) {
+    return Response.json(
+      { success: false, message: "Not Authenticated" },
+      { status: 401 }
+    );
+  }
+
+  try {
+    const response = await UserModel.updateOne(
+      { _id: user._id },
+      { $pull: { messages: { _id: messageId } } }
+    );
+
+    if (response.modifiedCount == 0) {
+      return Response.json(
+        { success: false, message: "Message not found or already deleted" },
+        { status: 404 }
+      );
+    }
+
+    return Response.json(
+      { success: true, message: "Message Deleted" },
+      { status: 201 }
+    );
+  } catch (error) {}
+}
